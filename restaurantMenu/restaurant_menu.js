57a0fb7d5bc6be5ed341f2349dfff81f670b5bbc
Fares <fareschehidi7@gmail.com>
2025-05-29 15:22:55 -0400

    Lab 5 Exercice completed

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,6 +1,7 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
-const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenu = ['Pancakes- $12.00', 'Eggs Benedict- $5.50', 'Oatmeal- $5.50', 'Frittata-$8.50'];
+const mainCourseMenu = ['Steak- $21.00', 'Pasta- $15.00', 'Burger- $13.50', 'Salmon- $18.99'];
+const dessertMenu = ['Cake- $6.00', 'Ice Cream- $8.00', 'Pudding- $7.50', 'Fruit Salad- $6.50'];
+
 
 const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
 document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
